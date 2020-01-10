ff5dbf6c3d3c369e69a5472494764a160502aa45
Votre Nom <odilecoutiez@gmail.com>
2020-01-10 16:06:42 +0100

    Exercice PHP sur les verification des champs des formulaires

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <title>Partie-IX</title>
+    </head>
+    <body>
+        <div class="page text-center bold">
+            <h1>PHP PARTIE X <span class="bold">"Travaux pratiques"</span></h1>
+            <div class="row">
+                <div class="col-md-12 text-center mt-3 mb-3">
+                    <p>Faire une page pour enregistrer un futur apprenant. On devra pouvoir saisir les informations suivantes :</p>
+                    <ul>
+                        <li>Nom</li> 
+                        <li>Prénom</li>
+                        <li>Date de naissance</li>
+                        <li>Pays de naissancev</li>
+                        <li>Nationalité</li>
+                        <li>Adresse</li>
+                        <li>Email</li>
+                        <li>Diplôme (sans, Bac, Bac+2, Bac+3 ou supérieur)</li>
+                        <li>Numéro pôle emploi</li>
+                        <li>Nombre de badge</li>
+                        <li>Liens codecademy</li>
+                    </ul>
+                    <p>Si vous étiez un super héros/une super héroïne, qui seriez-vous et pourquoi?</p>
+                    <p>Racontez-nous un de vos "hacks" (pas forcément technique ou informatique)</p>
+                    <p>Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?</p>
+
+                    <p>A la validation de ces informations, il faudra les afficher dans la même page à la place du formulaire.</p>
+                    <button type="button"><a href="tp-1/index.php">Tp-1</a></button>
+                    <button type="button"><a href="tp-1/tp1/index.php">Tp-1 Correction</a></button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center mt-3 mb-3">
+                    <p>Faire une page permettant de saisir les informations suivantes :</p>
+                     <p> Civilité (liste déroulante)</p>
+                    <ul>
+                        <li>Nom</li>
+                        <li>Prénom</li>
+                        <li>Age</li>
+                        <li>Société</li>
+                    </ul>
+
+                     <p>A la validation, les données saisies devront aparaitre sous le formulaire. Attention les données devront rester dans les différents éléments du formulaire même après la validation.</p>
+                    <button type="button"><a href="tp-2/index.php">Tp-2</a></button>
+                     <button type="button"><a href="tp-2/correc/index.php">correction-3</a></button>
+                </div> 
+            </div>
+             <div class="row">
+                <div class="col-md-12 text-center mt-3 mb-3">
+                    <p>Faire une fonction qui permet d'afficher les données des tableaux suivants :</p>
+                    <ul>
+                        <li>$portrait1 = array('name'=>'Victor', 'firstname'=>'Hugo', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');</li>
+                        <li>$portrait2 = array('name'=>'Jean', 'firstname'=>'de La Fontaine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');</li>
+                        <li>$portrait3 = array('name'=>'Pierre', 'firstname'=>'Corneille', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');</li>
+                        <li>$portrait4 = array('name'=>'Jean', 'firstname'=>'Racine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');</li>
+                    </ul>                 
+                    <p>Les afficher tous sur une même page.</p>
+                    <button type="button"><a href="tp-3/index.php">Tp-3</a></button> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
